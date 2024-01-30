650cb5525968f7c26f52a00fb1a3a8400fb7dfed
Sanju Balasuriya <sanjubalasuriya2@gmail.com>
2024-01-30 22:57:48 +0530

    update Cart Repo

--- src/main/java/com/nextevent/repository/CartRepo.java
+++ src/main/java/com/nextevent/repository/CartRepo.java
@@ -2,6 +2,8 @@ package com.nextevent.repository;
 
 import com.nextevent.entity.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CartRepo extends JpaRepository<Cart, Integer> {
 }
