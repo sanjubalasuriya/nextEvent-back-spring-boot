2dcc7ac53558f5bb22b3ff38058e0a6d22d2fb9c
Sanju Balasuriya <sanjubalasuriya2@gmail.com>
2024-02-07 20:31:21 +0530

    update CartRepo

--- src/main/java/com/nextevent/repository/CartRepo.java
+++ src/main/java/com/nextevent/repository/CartRepo.java
@@ -4,6 +4,10 @@ import com.nextevent.entity.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CartRepo extends JpaRepository<Cart, Integer> {
+
+    List<Cart> findAllByCustomer_CustomerId(int customerId);
 }
